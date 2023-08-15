fda80ad070af1c2194c79dafb4d520b52d13ed7a
rarito83 <rarito.jogja@gmail.com>
2023-08-15 14:34:47 +0700

    Initial Commit

---
+++ MovieBoxs/MovieBoxsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxsApp.swift
+//  MovieBoxs
+//
+//  Created by Rarito on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
